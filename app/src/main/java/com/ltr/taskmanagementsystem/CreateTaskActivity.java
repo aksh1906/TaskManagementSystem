96ba07529ad8a5f72e884ecf4bcd8d03fd5160eb
Akshat Sharma <akshat.sharma1906@gmail.com>
2018-01-27 16:34:15 +0530

    Removed some test code

--- app/src/main/java/com/ltr/taskmanagementsystem/CreateTaskActivity.java
+++ app/src/main/java/com/ltr/taskmanagementsystem/CreateTaskActivity.java
@@ -1,22 +1,7 @@
 package com.ltr.taskmanagementsystem;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewDebug;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class CreateTaskActivity extends AppCompatActivity {
 
@@ -24,61 +9,5 @@ public class CreateTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
-
-//        Spinner spinner = findViewById(R.id.spinnerTaskType);
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.array_task_types, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter);
-
-        final Spinner spinner = (Spinner) findViewById(R.id.spinnerTaskType);
-
-        String[] plants = new String[] {
-                "Select an item",
-                "California Sycamore",
-                "Mountain Mahogany",
-                "Butterfly Weed",
-                "Carrot Weed"
-        };
-
-        final List<String> plantsList = new ArrayList<>(Arrays.asList(plants));
-
-        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_create_task, plantsList) {
-            @Override
-            public boolean isEnabled(int position) {
-                if(position == 0) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-            @Override
-            public View getDropDownView(int position, View convertView, ViewGroup parent) {
-                View view = super.getDropDownView(position, convertView, parent);
-                TextView tv = (TextView) view;
-                if(position == 0) {
-                    tv.setTextColor(Color.GRAY);
-                } else {
-                    tv.setTextColor(Color.BLACK);
-                }
-                return view;
-            }
-        };
-        spinnerArrayAdapter.setDropDownViewResource(R.layout.activity_create_task);
-        spinner.setAdapter(spinnerArrayAdapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItemText = (String) parent.getItemAtPosition(position);
-                if(position > 0) {
-                    Toast.makeText(getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
     }
 }
