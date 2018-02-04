8e4ea00030e1e9a862901b1e8f5e08acb2dc5c3a
Akshat Sharma <akshat.sharma1906@gmail.com>
2018-02-04 16:17:05 +0530

    Added login validation Also added a few cosmetic changes

--- app/src/main/java/com/ltr/taskmanagementsystem/LoginActivity.java
+++ app/src/main/java/com/ltr/taskmanagementsystem/LoginActivity.java
@@ -1,37 +1,114 @@
 package com.ltr.taskmanagementsystem;
 
-import android.content.Intent;
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.PasswordTransformationMethod;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username, password;
+    private TextView incorrectDetails, forgotPassword;
+    private Button submitButton;
+
+    private TextWatcher usernameWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            checkUsernameIsEmpty();
+        }
+    };
+
+    private final TextWatcher passwordWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+            checkPasswordIsEmpty();
+        }
+    };
+
+    void checkUsernameIsEmpty() {
+
+        String uname = username.getText().toString();
+
+        if(uname.isEmpty()) {
+            username.setError("Username cannot be empty");
+        }
+        checkFieldsForEmptyValues();
+    }
+
+    void checkPasswordIsEmpty() {
+        String passwd = password.getText().toString();
+        if(passwd.isEmpty()) {
+            password.setError("Password cannot be empty");
+        }
+
+        checkFieldsForEmptyValues();
+    }
+//
+    void checkFieldsForEmptyValues() {
+        submitButton = findViewById(R.id.btnSubmit);
+
+        String uname = username.getText().toString();
+        String passwd = password.getText().toString();
+
+        if (uname.equals("") || passwd.equals("")) {
+            submitButton.setEnabled(false);
+        } else {
+            submitButton.setBackgroundResource(R.drawable.button_rounded_corners_enabled);
+            submitButton.setEnabled(true);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText password = (EditText) findViewById(R.id.etPassword);
-        password.setTransformationMethod(new PasswordTransformationMethod());
-    }
+        username = findViewById(R.id.etUsername);
+        password = findViewById(R.id.etPassword);
+        incorrectDetails = findViewById(R.id.tvIncorrectUsernamePassword);
+        forgotPassword = findViewById(R.id.tvForgotUsernamePassword);
+        submitButton = findViewById(R.id.btnSubmit);
 
-    public void login(View view) {
-        EditText username = (EditText) findViewById(R.id.etUsername);
-        EditText password = (EditText) findViewById(R.id.etPassword);
-        TextView incorrectDetails = (TextView) findViewById(R.id.tvIncorrectUsernamePassword);
-        TextView forgotPassword = (TextView) findViewById(R.id.tvForgotUsernamePassword);
+        username.addTextChangedListener(usernameWatcher);
+        password.addTextChangedListener(passwordWatcher);
 
-        if(username.getText().toString().equals("admin") & password.getText().toString().equals("password")) {
-            // goto other activity
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        } else {
-            incorrectDetails.setVisibility(View.VISIBLE);
-            forgotPassword.setVisibility(View.VISIBLE);
-        }
+        checkFieldsForEmptyValues();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // login
+            }
+        });
     }
 }
+
+
+
